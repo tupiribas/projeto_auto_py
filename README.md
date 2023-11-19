7e0fc3d4dc9e5210f8ba21694150d945c86973aa
tupiribas <tupiguedes@outlook.com>
2023-11-19 14:53:02 -0300

    Verificando instalação do python e criando executável

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Objetivo
 Criar projetos profissionais automatizados para devs iniciantes.
+
+## Requisitos do sistema:
+    1. Sitema Windows-64b
+    2. Conexão com a internet
+    3. Instalar o Python
+    
--- main.py
+++ main.py
@@ -1,50 +1,135 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
 from tkinter.filedialog import askdirectory
+from win32com.shell.shell import ShellExecuteEx
+# from platform import system
+
 
 from quadros.quadro import (
     mostrar_texto, clicar_no_botao, entrada_de_dados, clicar_no_botao_expassado)
 
-# Condições:
-#   1. Já tenha instalado o python na máquina dele
+
+def verificar_instacao_python(elemento: tk.Entry):
+    '''...'''
+    cmd = "python --version"
+    privilegios = "runas"  # Aumentar o nível de privilégios
+    terminal = "powershell.exe"  # Caminho do executável do power shell
+
+    if ShellExecuteEx(lpFile=terminal, lpParameters=cmd):
+        # Adicioanar mensagem ao quadro principal
+        elemento.insert(
+            tk.END, "O python já está instalado!\n")
+        elemento.update_idletasks()
+    else:
+        elemento.insert(
+            tk.END, "O python NÃO está instalado!\n")
+        elemento.update_idletasks()
+    # except ImportError:
+    #     # Adicioanar mensagem ao quadro principal
+    #     elemento.insert(
+    #         tk.END, "É só o inicio...\nEstamos fazendo a instalação do Python v-3.11.5")
+    #     elemento.update_idletasks()
+
+    #     cmd = "& python-3.11.5-amd64.exe"
+    #     privilegios = "runas"  # Aumentar o nível de privilégios
+    #     terminal = "powershell.exe"  # Caminho do executável do power shell
+
+    #     ShellExecuteEx(lpVerb=privilegios, lpFile=terminal, lpParameters=cmd)
+
+    #     # Adiciona a mensagem de sucesso ao quadro principal
+    #     elemento.insert(
+    #         tk.END, "\nPronto!\nPython instalado com sucesso!\n")
+    #     elemento.update_idletasks()
 
 
-# def verificar_venv_instalado():
-#     try:
-#         import venv
-#         msg = 'O módulo venv está instalado na sua máquina.'
-#     except ImportError:
-#         msg = "O módulo venv NÃO está instalado na sua máquina."
-#     messagebox.showinfo('Verificação do venv', msg)
+def criar_ambiente_virtual(caminho_arquivo: str, elemento: scrolledtext.ScrolledText):
+    '''
+    ## Criando o Ambienten Virtual
 
-def criar_arquivo_venv(caminho_arquivo, entidade: scrolledtext.ScrolledText):
-    import subprocess
-    comando = "python -m venv venv"
+    ### Atribuições
+    1. Criar uma pasta chamada .env que será o novo ambiente virtual do projeto python.\n
+    2. Obtendo como parâmetro o elemento e atualizando o status no texto de saída
+
+    parâmetros:
+        caminho_arquivo : str
+        elemento : scrolledtext.ScrolledText (widget caixa de texto grande)
+    return: void
+    '''
+    from subprocess import run, PIPE, CalledProcessError
+
+    cmd = "python -m venv .env"
     try:
         # Adicioanar mensagem ao quadro principal
-        entidade.insert(tk.END, "Iniciando a criação do ambiente virtual...\n")
-        entidade.update_idletasks()
+        elemento.insert(
+            tk.END, "Tenha paciência...\nEstou criando o ambiente virtual...\n")
+        elemento.update_idletasks()
 
-        subprocess.run(comando, cwd=caminho_arquivo, check=True,
-                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        run(cmd, cwd=caminho_arquivo, check=True, stdout=PIPE, stderr=PIPE)
 
         # Adiciona a mensagem de sucesso ao quadro principal
-        entidade.insert(tk.END, "Ambiente virtual criado com sucesso!\n")
-        entidade.update_idletasks()
+        elemento.insert(
+            tk.END, "\nPronto!\nAmbiente virtual criado com sucesso!\n")
+        elemento.update_idletasks()
         messagebox.showinfo(
             "Sucesso!", "Ambiente virtual criado com sucesso!")
-    except subprocess.CalledProcessError:
+    except CalledProcessError:
         messagebox.showerror(
-            "Erro", "Ocorreu um erro ao executar a operação de executar o venv.")
+            "Erro", "Ocorreu um erro ao executar a operação de criar pasta venv.")
 
 
-def instanciar_projeto(caminho_do_projeto, entidades: list[tk.Tk]):
-    criar_arquivo_venv(caminho_arquivo=caminho_do_projeto,
-                       entidade=entidades[0])
+def ativar_politicas_execucao(elemento: scrolledtext.ScrolledText):
+    '''## Politicas Execução no Windows
+    Comando executado no shell como adm:\n
+    >>> "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine"
+
+    ### Atribuições
+    1. Permite que o usuário possa executar a ativação do ambiente virutal (.env).
+    2. Obtendo como parâmetro o elemento e atualizando o status no texto de saída
+
+    parâmetro:
+        elemento : scrolledtext.ScrolledText (widget caixa de texto grande)
+    return: void
+    '''
+    from win32com.shell.shell import ShellExecuteEx
+    # Adicioanar mensagem ao quadro principal
+    elemento.insert(
+        tk.END, "\nAtivando políticas de execução")
+    elemento.update_idletasks()
+
+    cmd = "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine"
+    privilegios = "runas"  # Aumentar o nível de privilégios
+    terminal = "powershell.exe"  # Caminho do executável do power shell
+
+    ShellExecuteEx(lpVerb=privilegios, lpFile=terminal, lpParameters=cmd)
+
+    # Adiciona a mensagem de sucesso ao quadro principal
+    elemento.insert(
+        tk.END, "\nPronto!\nPolíticas de execução ativadas!\n")
+    elemento.update_idletasks()
 
 
-def buscar_caminho_pasta(elemento: tk.Entry) -> str:
-    '''Encontrar caminho da pasta do novo projeto'''
+def instancias_do_projeto(caminho_do_projeto: str, *elementos: tk.Tk):
+    '''Cria uma sequencia de tarefas para criar o projeto.
+
+    parâmetros:
+        caminho_do_projeto : str -> caminho da pasta do projeto
+        elementos : list[tkinter] -> elemento qualquer que vai receber ou que vai enviar 
+    return void
+    '''
+    verificar_instacao_python(elemento=elementos[0])
+    criar_ambiente_virtual(caminho_arquivo=caminho_do_projeto,
+                           elemento=elementos[0])
+    ativar_politicas_execucao(elemento=elementos[0])
+
+
+def selecionar_caminho_pasta(elemento: tk.Entry) -> str:
+    '''Seleciona caminho da pasta para criar um novo novo projeto
+
+    parâmetros:
+        elemento : tkinter.Entry
+
+    return pasta_selecionada : str
+    '''
     try:
         pasta_selecionada = askdirectory()
         elemento.delete(0, tk.END)  # Limpa o conteúdo atual do Entry
@@ -58,19 +143,20 @@ def main():
     '''Programa Principal'''
     # Criar Janela principal
     janela = tk.Tk()
-    janela.title("Criar Projeto")
+    janela.title("Criar Projeto Python")
 
+    # Cria um quadro (frame) dentro da janela princial
     quadro = tk.Frame(janela)
     quadro.pack(padx=20, pady=20)
 
     # Obter o caminho da pasta do novo projeto
     mostrar_texto(
-        "1. Informe o local do arquivo onde localiza a pasta do novo projeto:", quadro)
-    entidade_caminho_pasta = entrada_de_dados(50, quadro)
+        "Informe a pasta do novo projeto:", quadro)
+    elemento_caminho_pasta = entrada_de_dados(50, quadro)
     clicar_no_botao(
-        "Procurar Pasta...", lambda: buscar_caminho_pasta(entidade_caminho_pasta), quadro)
+        "Procurar Pasta...", lambda: selecionar_caminho_pasta(elemento_caminho_pasta), quadro)
 
-    # Preparar pasta para projeto base python
+    # Visualização do processo de instrâcia do novo projeto
     texto_de_saida = scrolledtext.ScrolledText(
         janela, wrap=tk.WORD, width=40, height=10)
     texto_de_saida.pack()
@@ -80,11 +166,10 @@ def main():
         titulo="Cancelar", largura=5, altura=5, comando=janela.destroy, quadro=janela)
     clicar_no_botao_expassado(
         titulo="Enviar", largura=5, altura=5,
-        comando=lambda: instanciar_projeto(entidade_caminho_pasta.get(), [texto_de_saida]), quadro=janela)
+        comando=lambda: instancias_do_projeto(elemento_caminho_pasta.get(), texto_de_saida), quadro=janela)
 
     janela.mainloop()
 
 
 if __name__ == "__main__":
     main()
-    # verificar_venv_instalado()
